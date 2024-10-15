cbf23d34ffaff8d1cc37a268eb7e231fbfcf3ac4
li-maksim <max.mail4@yandex.ru>
2024-10-15 20:06:43 +0300

    Added buttons to delete Projects

--- src/index.js
+++ src/index.js
@@ -40,19 +40,38 @@ const ProjectMgmt = (() => {
 const Display = (() => {
     const content = document.querySelector('#content');
     const projMenu = document.querySelector('#proj_menu');
-
+    let idxNum = -1;
 
     function createMenuItem(v, i, a) {
+        idxNum++;
         const newMenuItem = document.createElement('div');
         newMenuItem.setAttribute('class', 'menu_item');
-        newMenuItem.textContent = a[i].name;
+        const projName = document.createElement('div');
+        projName.setAttribute('class', 'project_name');
+        projName.textContent = a[i].name;
+
+        const projDelBtn = document.createElement('button');
+        projDelBtn.classList.add('btn');
+        projDelBtn.classList.add('icon-delete');
+        projDelBtn.setAttribute('data-project', idxNum);
+        function delProj() {
+            ProjectMgmt.delProj(projDelBtn.dataset.project);
+            console.log(projDelBtn.dataset.project);
+        };
+        projDelBtn.addEventListener('click', delProj);
+        projDelBtn.addEventListener('click', displayProjects);
+
+        newMenuItem.appendChild(projName);
+        newMenuItem.appendChild(projDelBtn);
         projMenu.appendChild(newMenuItem);
     };
+
     function clearMenu() {
         projMenu.textContent = '';
     };
     function displayProjects() {
         clearMenu();
+        idxNum = -1;
         Projects.forEach(createMenuItem);
     };
 
@@ -66,7 +85,3 @@ ProjectMgmt.addProj('Three');
 ProjectMgmt.addProj('Four');
 console.table(Projects);
 Display.displayProjects();
-
-ProjectMgmt.delProj(2);
-Display.displayProjects();
-
