59ef1dfe604ad3897b684c856692a510c326fa45
li-maksim <max.mail4@yandex.ru>
2024-10-17 19:13:46 +0300

    Added button to create new projects
    Added dialog for project creation in html and functions in js to create projects

--- src/index.js
+++ src/index.js
@@ -46,6 +46,35 @@ const Display = (() => {
     const projMenu = document.querySelector('#proj_menu');
     let idxNum = -1;
 
+    function clearMenu() {
+        projMenu.textContent = '';
+    };
+    function displayProjects() {
+        clearMenu();
+        idxNum = -1;
+        Projects.arr.forEach(createMenuItem);
+    };
+
+    const Windows = (() => {
+        const projNewBtn = document.querySelectorAll('.new_proj');
+
+        const projAddWindow = document.querySelector('.proj_addwindow');
+        const projAddInput = document.querySelector('#proj_add');
+        const projAddBtn = document.querySelectorAll('.proj_addbtn');
+        function addProj(evt) {
+            evt.preventDefault();
+            Project(projAddInput.value);
+            displayProjects();
+            projAddWindow.close();
+        };
+        function showAddWindow() {
+            projAddWindow.showModal();
+        }
+
+        projAddBtn.forEach(function(a) {a.addEventListener('click', addProj)});
+        projNewBtn.forEach(function(a) {a.addEventListener('click', showAddWindow)});
+    })();
+
     function createMenuItem(v, i, a) {
         idxNum++;
         const newMenuItem = document.createElement('div');
@@ -53,6 +82,13 @@ const Display = (() => {
         const projName = document.createElement('div');
         projName.setAttribute('class', 'project_name');
         projName.textContent = a[i].name;
+
+        const itemIcons = document.createElement('div');
+        itemIcons.setAttribute('class', 'item_icons');
+
+        const projEditBtn = document.createElement('button');
+        projEditBtn.classList.add('btn');
+        projEditBtn.classList.add('icon-edit');
 
         const projDelBtn = document.createElement('button');
         projDelBtn.classList.add('btn');
@@ -65,18 +101,12 @@ const Display = (() => {
         projDelBtn.addEventListener('click', delProj);
         projDelBtn.addEventListener('click', displayProjects);
 
-        newMenuItem.appendChild(projName);
-        newMenuItem.appendChild(projDelBtn);
-        projMenu.appendChild(newMenuItem);
-    };
+        itemIcons.appendChild(projEditBtn);
+        itemIcons.appendChild(projDelBtn);
 
-    function clearMenu() {
-        projMenu.textContent = '';
-    };
-    function displayProjects() {
-        clearMenu();
-        idxNum = -1;
-        Projects.arr.forEach(createMenuItem);
+        newMenuItem.appendChild(projName);
+        newMenuItem.appendChild(itemIcons);
+        projMenu.appendChild(newMenuItem);
     };
 
     return {clearMenu, displayProjects};
